748da50bb9f47217507a8896b1ffd3e57fcf6563
Alon Altman <github@8ln.org>
2020-12-06 21:44:55 -0800

    day 7

--- 2020/7.pl
+++ 2020/7.pl
@@ -1,27 +1,66 @@
 #!/usr/bin/perl -w
 use strict;
 
-my $c = 0;
+my %BAG;
 
-my $qc = 0;
-
-my %Q;
-
-sub onEnd {
-  # CODE GOES HERE
-  %Q = (); $qc = -1;
+sub DFS {
+  my ($cur,$seen,$target) = @_;
+  print "CUR = $cur SEEN = ".join(',', keys %$seen)."\n";
+  $seen->{$cur} = 1;
+  my $nexts = $BAG{$cur} || [];
+  for my $n (@$nexts) {
+    next if ($seen->{$n});
+    if ($n eq $target) {
+      return 1;
+    }
+    if (DFS($n, $seen, $target)) {
+      return 1;
+    }
+  }
+  return 0;
 }
+
+sub DFSCount {
+  my ($cur,$seen) = @_;
+  print "CUR = $cur SEEN = ".join(',', keys %$seen)."\n";
+  #$seen->{$cur} = 1;
+  my $nexts = $BAG{$cur} || [];
+  my $sum = 1;
+  for my $n (@$nexts) {
+    my ($nb, $cnt) = @$n;
+    $sum += $cnt * DFSCount($nb, $seen)
+  }
+  return $sum;
+}
+
 
 while (<>) {
   print;
   chomp;
-  # CODE GOES HERE
-  if (m/^$/o) {
-    onEnd();
+  unless (m{^([\w\s]+\w)\s+bags contain (.+)\.$}o) {
+    print "ERR $_\n";
   }
-  $qc++;
+  next if ($2 eq "no other bags");
+  my $out = $1;
+  my @inbags = split(/,\s*/,$2);
+  my @ret;
+  for my $inbag (@inbags) {
+    $inbag =~ m{(\d+) ([\w\s]+\w) bag(s?)};
+    push @ret, [$2, $1];
+  }
+  $BAG{$out} = \@ret;
 }
 
-onEnd();
+while (my ($k,$v) = each %BAG) {
+  print "$k => ", join(',', @$v), "\n";
+}
 
-print "$c\n";
+#my $c = 0;
+
+#for my $b (keys %BAG) {
+#  $c += DFS($b, {}, "shiny gold")
+#}
+
+# print "$c\n";
+
+print ((DFSCount("shiny gold", {})-1)."\n");
