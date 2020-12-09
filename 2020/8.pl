8df907b24879088a46b6adb8e74905cae0bb13a8
Alon Altman <github@8ln.org>
2020-12-08 20:54:33 -0800

    O(n) implementation for day 8

--- 2020/8.pl
+++ 2020/8.pl
@@ -34,17 +34,55 @@ sub run {
   return "fail";
 }
 
+my @REV;
+
 for my $i (0..$#PROG) {
-  next if ($PROG[$i] =~ /acc/);
-  my @tmp = @PROG;
-  $_ = $PROG[$i];
-  s/jmp/tmp/;
-  s/nop/jmp/;
-  s/tmp/nop/;
-  $tmp[$i] = $_;
-  my $res = run(@tmp);
-  if ($res ne 'fail') {
-    print "$res\n";
+  my $l = $PROG[$i];
+  $l =~ m{^(\w+)\s+([-+]\d+)$};
+  my ($opc, $arg) = ($1, $2);
+  my $target = $i + 1;
+  if ($opc eq "jmp") {
+    $target = $i + $arg;
+  }
+  push @{$REV[$target]}, $i;
+}
+
+my %endzone;
+my @q = ($#PROG + 1);
+while (@q) {
+  my $i = shift @q;
+  $endzone{$i}++;
+  for my $r (@{$REV[$i]}) {
+    if (!$endzone{$r}) {
+      push @q, $r;
+    }
   }
 }
 
+my %SEEN;
+my $pc = 0;
+my $acc = 0;
+
+while (!$SEEN{$pc}) {
+  $SEEN{$pc}++;
+  if ($pc > $#PROG) {
+    print "$acc\n";
+    exit;
+  }
+  my $inst = $PROG[$pc];
+  $inst =~ m{^(\w+)\s+([-+]\d+)$};
+  my $alt = $pc + 1;
+  my ($opc, $arg) = ($1, $2);
+  if ($opc eq "acc") {
+    $acc += $arg;
+  } elsif ($opc eq "jmp") {
+    $pc += $arg - 1;
+  } elsif ($opc eq "nop") {
+    $alt = $pc + $arg;
+  }
+  $pc++;
+  if (!$endzone{$pc} && $endzone{$alt}) {
+    print "Set PC from $pc to $alt\n";
+    $pc = $alt;
+  }
+}
