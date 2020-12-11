60d22e2c570e617e90cded521fff4929e697182c
Alon Altman <github@8ln.org>
2020-12-10 21:58:02 -0800

    day11

--- 2020/11.pl
+++ 2020/11.pl
@@ -17,6 +17,8 @@ my @neigh = ([-1,-1], [-1,0], [-1,1], [0,-1], [0, 1], [1,-1], [1,0], [1,1]);
 
 my @m = map {my @r = split(//); \@r} @A;
 
+my %viz;
+
 my $ch =1;
 while ($ch) {
   $ch = 0;
@@ -30,28 +32,41 @@ while ($ch) {
       #print "RC $r $c $cv\n";
       next if ($cv eq '.');
       my $cnt = 0;
-      for my $n (@neigh) {
+      BIG: for my $n (@neigh) {
         my ($rd, $cd) = @$n;
-        my $rn = $r + $rd;
-        next if ($rn < 0 || $rn > $#m);
-        my $cn = $c + $cd;
-        next if ($cn < 0 || $cn > $#$rv);
-        #print "rncn $rn $cn ",$m[$rn][$cn], "\n";
-        $cnt++ if $m[$rn][$cn] eq '#';
+        my $d = 1;
+        my ($rn,$cn);
+        my $cur;
+        if ($viz{"$r,$c,$rd,$cd"}) {
+          ($rn,$cn) = @{$viz{"$r,$c,$rd,$cd"}};
+          $cur = $m[$rn][$cn];
+        } else {
+          do {
+            $rn = $r + $rd * $d;
+            next BIG if ($rn < 0 || $rn > $#m);
+            $cn = $c + $cd * $d;
+            next BIG if ($cn < 0 || $cn > $#$rv);
+            $d++;
+            #print "rncn = $rn $cn\n";
+            $cur = $m[$rn][$cn];
+          } while ($cur eq '.');
+          $viz{"$r,$c,$rd,$cd"} = [$rn, $cn];
+        }
+        $cnt++ if $cur eq '#';
       }
       #print "$r $c $cnt\n";
       if ($cnt == 0 && $cv eq 'L') {
         $new[$r][$c] = '#';
         $ch++;
       }
-      if ($cnt >= 4 && $cv eq '#') {
+      if ($cnt >= 5 && $cv eq '#') {
         $new[$r][$c] = 'L';
         $ch++;
       }
     }
   }
   @m = @new;
-  print join("\n", map {join ('', @$_)} @m);
+  print join("\n", map {join ('', @$_)} @m), "\n\n$ch\n\n";
 }
 
 my $count = 0;
