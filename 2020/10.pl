cdbd8cc61d4efd2803259d3b2d5a904e29e24907
Alon Altman <github@8ln.org>
2020-12-10 20:39:02 -0800

    defined

--- 2020/10.pl
+++ 2020/10.pl
@@ -15,16 +15,12 @@ my @jolts = sort {$a <=> $b} @A;
 push @jolts, $jolts[$#jolts] + 3;
 unshift @jolts, 0;
 
-my $l = 0;
-
-my @C;
-
 my %MEMO;
 $MEMO{$#jolts} = 1;
 
 sub cnt {
   my $i = shift;
-  return $MEMO{$i} if $MEMO{$i};
+  return $MEMO{$i} if defined($MEMO{$i});
   my $sum = 0;
   for my $s (1..3) {
     last if ($i + $s > $#jolts); 
