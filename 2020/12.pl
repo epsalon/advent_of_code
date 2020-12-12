a37bcc92f8c6fcff5a99548cc9adcc4df01995ec
Alon Altman <github@8ln.org>
2020-12-11 23:17:21 -0800

    now with fewer lines

--- 2020/12.pl
+++ 2020/12.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -w
 use strict;
-use Data::Dumper;
 use Math::Complex;
 
 my %DIRS = ('N', -1, 'S', 1, 'E', i, 'W', -i);
@@ -8,9 +7,7 @@ my $dir = -1 + 10 * i;
 my $pos = 0;
 
 while (<>) {
-  print;
-  chomp;
-  /^([NEWSLRF])(\d+)$/ or die;
+  /^([NEWSLRF])(\d+)/ or die;
   my ($cmd,$val) = ($1,$2);
   if ($cmd eq 'F') {
     $pos+=$dir * $val;
@@ -21,7 +18,6 @@ while (<>) {
       $val = -$val;
     }
     $dir *= i ** ($val/90);
-    print "$dir\n";
   }
 }
 
