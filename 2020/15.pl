107b73e0be42377b752d448fa4d87c503aa9e1cb
Alon Altman <github@8ln.org>
2020-12-14 21:38:23 -0800

    Day 15

---
+++ 2020/15.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+use strict;
+use Data::Dumper;
+use feature 'say';
+use Clipboard;
+
+sub out {
+  my $out = shift;
+  Clipboard->copy_to_all_selections($out);
+  print "$out\n";
+}
+
+$_=<>;
+chomp;
+my @A = split(/,/);
+
+my $prev = pop @A;
+
+my $N=0;
+
+my %LASTIDX;
+for my $i (0..$#A) {
+  $LASTIDX{$A[$i]}=$i;
+  $N++;
+}
+
+BIG: while ($N < 30000000 - 1) {
+  say "$N" unless ($N % 1000000);
+  my $prevn = $LASTIDX{$prev};
+  say "prev=$prev";
+  $LASTIDX{$prev} = $N;
+  #say "lastn=$lastn prevn=$prevn";
+  if (!defined($prevn)) {
+    $prev = 0;
+  } else {
+    #say "prevn = $prevn N = $N";
+    $prev = $N-$prevn;
+  }
+  $N++;
+}
+
+out ($prev);
