92178b245dbf63a4d9c532358efea5b8cbe9f21b
Alon Altman <github@8ln.org>
2021-12-20 21:59:56 -0800

    clean up output

--- 2021/21.pl
+++ 2021/21.pl
@@ -47,25 +47,32 @@ sub ucount {
 
 memoize('ucount');
 
-my $p1 = 10;
-my $p2 = 3;
+sub part1 {
+  my ($p1,$p2) = @_;
+  my ($s1,$s2) = (0,0);
+  my $d=1;
 
-out(max(ucount(3,4,8,0,0)));
-out(max(ucount(3,10,3,0,0)));
-
-my ($s1,$s2) = (0,0);
-my $d=1;
-
-while ($s1 < 1000 && $s2 < 1000) {
-  $p1 += ($d++) % 100 || 100;
-  $p1 += ($d++) % 100 || 100;
-  $p1 += ($d++) % 100 || 100;
-  $p1 = $p1 % 10 || 10;
-  $s1 += $p1;
-  last if ($s1 >= 1000);
-  $p2 += ($d+1)*3; $d+=3;
-  $p2 = $p2 % 10 || 10;
-  $s2 += $p2;
+  while ($s1 < 1000 && $s2 < 1000) {
+    $p1 += ($d++) % 100 || 100;
+    $p1 += ($d++) % 100 || 100;
+    $p1 += ($d++) % 100 || 100;
+    $p1 = $p1 % 10 || 10;
+    $s1 += $p1;
+    last if ($s1 >= 1000);
+    $p2 += ($d+1)*3; $d+=3;
+    $p2 = $p2 % 10 || 10;
+    $s2 += $p2;
+  }
+  return ($d-1) * min($s1,$s2);
 }
 
-out(($d-1) * min($s1,$s2));
+print "Part 1 result for example: ";
+out(part1(4,8));
+print "Part 1 result for real input: ";
+out(part1(10,3));
+
+print "Part 2 result for example: ";
+out(max(ucount(3,4,8,0,0)));
+print "Part 2 result for real input: ";
+out(max(ucount(3,10,3,0,0)));
+
