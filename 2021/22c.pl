f027a6be1c5879eb893a0dbf3eb55a418d7fec72
Alon Altman <github@8ln.org>
2021-12-22 01:55:26 -0800

    even more efficient

--- 2021/22c.pl
+++ 2021/22c.pl
@@ -21,27 +21,28 @@ sub printlist {
   say join('; ', map {join(',', @$_)} @_), " => ", sum(map {volume($_)} @_);
 }
 
+# May destroy p1
 sub prismdiff {
   my ($p1,$p2) = @_;
-  $p1 = [@$p1];
-  $p2 = [@$p2];
+  my @pre;
+  for my $i (0..@$p1/2-1) {
+    my $x1 = max($p1->[2*$i], $p2->[2*$i]);
+    my $x2 = min($p1->[2*$i + 1], $p2->[2*$i + 1]);
+    return $p1 unless ($x1 < $x2);
+    push @pre, ($x1, $x2);
+  }
   my @out;
   my @prefix;
   while (@$p1) {
-    my ($s1,$e1,$s2,$e2) = (shift @$p1, shift @$p1, shift @$p2, shift @$p2);
+    my ($s1,$e1,$s2,$e2) = (splice(@$p1,0,2), $p2->[@prefix], $p2->[@prefix+1]);
     if ($s1 < $s2) {
-      push @out,[@prefix, $s1,min($e1,$s2), @$p1];
+      push @out,[@prefix, $s1, min($e1,$s2), @$p1];
     }
     if ($e2 < $e1) {
-      push @out,[@prefix, max($s1,$e2),$e1, @$p1];
-    }
-    my $x1 = max($s1, $s2);
-    my $x2 = min($e1, $e2);
-    unless ($x1 < $x2) {
-      return $_[0];
+      push @out,[@prefix, max($s1,$e2), $e1, @$p1];
     }
     last unless @$p1;
-    push @prefix, ($x1, $x2);
+    push @prefix, splice(@pre,0,2);
   }
   return @out;
 }
