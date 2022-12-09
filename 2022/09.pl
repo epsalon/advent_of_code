3bc77ddebda016cdd88938fbe0953237d66a364e
Alon Altman <github@8ln.org>
2022-12-08 21:28:31 -0800

    day 9

---
+++ 2022/09.pl
@@ -0,0 +1,233 @@
+#!/usr/bin/perl -w
+use strict;
+no warnings 'portable';
+use Data::Dumper;
+use feature 'say';
+use Clipboard;
+use List::Util qw/sum min max reduce any all none notall first product uniq/;
+use Math::Cartesian::Product;
+use Math::Complex;
+use List::PriorityQueue;
+use Memoize;
+
+sub out {
+  my $out = shift;
+  if (ref($out)) {
+    print Dumper($out);
+  } else {
+    Clipboard->copy_to_all_selections($out);
+    print "$out\n";
+  }
+}
+
+# Utility function to make sure 2d array has equal rows
+# input arr,fill. no output
+sub equalize {
+  my $arr = shift;
+  my $fill = shift;
+  my $rows = @$arr;
+  my $cols = max(map {$_ ? scalar(@$_): 0} @$arr);
+  print "$rows $cols\n";
+  for my $row (@$arr) {
+    $row = [] unless $row;
+    while (@$row < $cols) {
+      push @$row, $fill;
+    }
+  }
+}
+
+# Find neighbors
+# input neigh_arr, arr, row, col
+# OR: neigh_arr, arr, "row,col"
+# OR: neigh_arr, rows, cols, row, col
+# OR: neigh_arr, rows, cols, "row,col"
+# returns array of [row, col, value]
+# OR: array of ["row,col", value]
+# OR: array of "row,col"
+sub neigh {
+  my $neigh = shift;
+  my ($rows,$cols);
+  my $arr = shift;
+  if (ref $arr) {
+    $rows = @$arr;
+    $cols = @{$arr->[0]};
+  } else {
+    $rows = $arr;
+    $cols = shift;
+    undef $arr;
+  }
+  my $row = shift;
+  my $col = shift;
+  my $comma;
+  if ($row =~ /(\d+)(\D+)(\d+)/) {
+    ($row, $comma, $col) = ($1, $2, $3);
+  }
+  my @out;
+  for my $pair (@$neigh) {
+    my ($rd, $cd) = @$pair;
+    my ($nr, $nc) = ($row + $rd, $col + $cd);
+    next if $nr < 0;
+    next if $nc < 0;
+    next if $nr >= $rows;
+    next if $nc >= $cols;
+    if (defined($comma)) {
+      if ($arr) {
+        push @out, ["$nr$comma$nc", $arr->[$nr][$nc]];
+      } else {
+        push @out, "$nr$comma$nc";
+      }
+    } else {
+      push @out, [$nr, $nc, $arr ? ($arr->[$nr][$nc],) : ()];
+    }
+  }
+  return @out;
+}
+
+# Orthogonal
+sub oneigh {
+  return neigh([[-1,0], [1, 0], [0, -1], [0, 1]], @_);
+}
+
+# All neighbors
+sub aneigh {
+  return neigh([
+    [-1, -1], [-1, 0], [-1, 1],
+    [ 0, -1],          [ 0, 1],
+    [ 1, -1], [ 1, 0], [ 1, 1]], @_);
+}
+
+# Numeric sort because sort defaults to lex
+# returns new array
+sub nsort {
+  my $in = \@_;
+  if (@$in == 1) {
+    $in = $in->[0];
+  }
+  return sort {$a <=> $b} @$in;
+}
+
+# Binary conversions
+sub bin2dec {
+  my $in = shift;
+  return oct("0b$in");
+}
+sub dec2bin {
+  my $in = shift;
+  return sprintf ("%b", $in);
+}
+sub hex2bin {
+  my $in = shift;
+  return join('', map {sprintf("%04b", oct("0x$_"))} split('', $in));
+}
+sub bin2hex {
+  my $in = shift;
+  my $out;
+  my @in = split('', $in);
+  die "Bad bin value $in" if (@in%4);
+  while (@in) {
+    $out .= sprintf("%X", bin2dec(join('',splice(@in, 0, 4))));
+  }
+  return $out;
+}
+
+# A* / BFS implementation
+# Args: start, end, neighbor function, heuristic function
+# neighbor function: node -> [[new_node, cost], ...]
+#   cost assumed 1 if missing
+# heuristic function: node -> lower bound on cost to end
+sub astar {
+  my ($start, $end, $neigh, $h) = @_;
+  $h = sub {return 0;} unless $h;
+
+  my $OPEN = new List::PriorityQueue;
+  my %gscore = ($start, 0);
+  my %OHASH = ($start, 1);
+  my %path;
+  $OPEN->insert($start, $h->($start));
+
+  while (%OHASH) {
+    my $cur = $OPEN->pop();
+    delete $OHASH{$cur};
+    if ($cur eq $end) {
+      my $score = $gscore{$cur};
+      return $score unless wantarray;
+      my @path = ($cur);
+      while ($cur = $path{$cur}) {
+        unshift(@path, $cur)
+      }
+      return ($score, @path);
+    }
+    for my $n ($neigh->($cur)) {
+      my ($np,$v) = @$n;
+      if (!defined($v)) {
+        $v = 1;
+      }
+      my $new_g = $gscore{$cur} + $v;
+      if (!exists($gscore{$np}) || $new_g < $gscore{$np}) {
+        $path{$np} = $cur if wantarray;
+        $gscore{$np} = $new_g;
+        my $fscore = $new_g + $h->($np);
+        if (!$OHASH{$np}) {
+          $OPEN->insert($np, $fscore);
+          $OHASH{$np}++;
+        } else {
+          $OPEN->update($np, $fscore);
+        }
+      }
+    }
+  }
+}
+
+sub smart_split {
+  my $str = shift || $_;
+  return ($str =~ m{[a-zA-Z]+|\d+}go);
+}
+
+sub hashify {
+  my @arr = ref $_[0] ? @{$_[0]} : @_;
+  return map {$_ => 1} @arr;
+}
+
+my @A;
+my %H;
+my $sum=0;
+
+my ($hr,$hc) = (0,0);
+
+my @T;
+
+for my $i (1..9) {push @T, [0,0]};
+
+my %dir = (U=>[-1,0], D=>[1,0], L=>[0,-1], R=>[0, 1]);
+
+sub sgn {
+  my $s=shift;
+  return -1 if ($s < 0);
+  return 1 if ($s > 0);
+  return 0;
+}
+
+while (<>) {
+  chomp;
+  last unless $_;
+  push @A, [split('')];
+  my @p = smart_split();
+  my ($a,$b) = @p;
+  my $d = $dir{$a};
+  #out($d);
+  for my $i (1..$b) {
+    $hr += $d->[0]; $hc += $d->[1];
+    my ($pr, $pc) = ($hr,$hc);
+    for my $t (@T) {
+      if (abs($pr-$t->[0]) > 1 || abs($pc-$t->[1]) > 1) {
+        $t->[0] += sgn($pr-$t->[0]);
+        $t->[1] += sgn($pc-$t->[1]);
+      }
+      ($pr,$pc) = @$t;
+    }
+    my ($tr,$tc) = @{$T[$#T]};
+    $H{"$tr,$tc"}++;
+  }
+}
+
+out (scalar(keys %H));
