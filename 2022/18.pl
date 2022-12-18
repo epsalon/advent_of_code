f3022372ec101a1e5b3e83f13c1c5f704c4854c4
Alon Altman <github@8ln.org>
2022-12-17 21:29:48 -0800

    day 18

---
+++ 2022/18.pl
@@ -0,0 +1,222 @@
+#!/usr/bin/perl -w
+use strict;
+no warnings 'portable';
+use Data::Dumper;
+use feature 'say';
+use Clipboard;
+use List::Util qw/sum min max reduce any all none notall first product uniq/;
+use Math::Cartesian::Product;
+use Math::Complex;
+use List::PriorityQueue;
+use Memoize;
+use Storable qw(dclone);
+
+sub out {
+  my $out = shift;
+  if (ref($out)) {
+    print Dumper($out);
+  } else {
+    Clipboard->copy_to_all_selections($out);
+    print "$out\n";
+  }
+}
+
+# Utility function to make sure 2d array has equal rows
+# input arr,fill. no output
+sub equalize {
+  my $arr = shift;
+  my $fill = shift;
+  my $rows = @$arr;
+  my $cols = max(map {$_ ? scalar(@$_): 0} @$arr);
+  print "$rows $cols\n";
+  for my $row (@$arr) {
+    $row = [] unless $row;
+    while (@$row < $cols) {
+      push @$row, $fill;
+    }
+  }
+}
+
+
+# Numeric sort because sort defaults to lex
+# returns new array
+sub nsort {
+  my $in = \@_;
+  if (@$in == 1) {
+    $in = $in->[0];
+  }
+  return sort {$a <=> $b} @$in;
+}
+
+# Binary conversions
+sub bin2dec {
+  my $in = shift;
+  return oct("0b$in");
+}
+sub dec2bin {
+  my $in = shift;
+  return sprintf ("%b", $in);
+}
+sub hex2bin {
+  my $in = shift;
+  return join('', map {sprintf("%04b", oct("0x$_"))} split('', $in));
+}
+sub bin2hex {
+  my $in = shift;
+  my $out;
+  my @in = split('', $in);
+  die "Bad bin value $in" if (@in%4);
+  while (@in) {
+    $out .= sprintf("%X", bin2dec(join('',splice(@in, 0, 4))));
+  }
+  return $out;
+}
+
+# A* / BFS implementation
+# Args: start, end, neighbor function, heuristic function
+#  - start is either a node or a list of nodes to start at.
+#  - end is either a node or a function that takes a single node
+#    and returns true/false.
+# - neighbor function: node -> ([new_node, cost], ...)
+#   - cost assumed 1 if missing
+# - heuristic function: node -> lower bound on cost to end (optional)
+sub astar {
+  my ($start, $end, $neigh, $h) = @_;
+  # Generalize parameters
+  $start = [$start] unless (ref($start) eq 'ARRAY');
+  if (ref($end) ne 'CODE') {
+    my $end_node = $end;
+    $end = sub { return $_[0] eq $end_node; };
+  }
+  die "bad neigh func $neigh" unless (ref($neigh) eq 'CODE');
+  $h = sub {return 0;} unless $h;
+
+  # Initialize open list
+  my $OPEN = new List::PriorityQueue;
+  my %gscore;
+  my %OHASH;
+  for my $s (@$start) {
+    $gscore{$s} = 0;
+    $OHASH{$s} = 1;
+    $OPEN->insert($s, $h->($s));
+  }
+
+  my %path;
+  while (%OHASH) {
+    my $cur = $OPEN->pop();
+    delete $OHASH{$cur};
+    # Check for end
+    if ($end->($cur)) {
+      say "reached end at $cur";
+      my $score = $gscore{$cur};
+      return $score unless wantarray;
+      my @path = ($cur);
+      while ($cur = $path{$cur}) {
+        unshift(@path, $cur)
+      }
+      return ($score, @path);
+    }
+    # Expand neighbors
+    for my $n ($neigh->($cur)) {
+      my ($np,$v);
+      if (ref($n) eq 'ARRAY') {
+        ($np,$v) = @$n;
+      } else {
+        $np = $n;
+      }
+      if (!defined($v)) {
+        $v = 1;
+      }
+      my $new_g = $gscore{$cur} + $v;
+      if (!exists($gscore{$np}) || $new_g < $gscore{$np}) {
+        # Found better path to $np
+        $path{$np} = $cur if wantarray;
+        $gscore{$np} = $new_g;
+        my $fscore = $new_g + $h->($np);
+        if (!$OHASH{$np}) {
+          $OPEN->insert($np, $fscore);
+          $OHASH{$np}++;
+        } else {
+          $OPEN->update($np, $fscore);
+        }
+      }
+    }
+  }
+}
+
+sub smart_split {
+  my $str = shift || $_;
+  return ($str =~ m{[a-zA-Z]+|\d+}go);
+}
+
+sub hashify {
+  my @arr = ref $_[0] ? @{$_[0]} : @_;
+  return map {$_ => 1} @arr;
+}
+
+my @A;
+my %H;
+my $sum=0;
+
+my (%XX,%YY,%ZZ);
+
+my ($min,$max)=(0,-99);
+
+while (<>) {
+  chomp;
+  last unless $_;
+  push @A, [split('')];
+  my @p = smart_split();
+  my ($x,$y,$z) = @p;
+  for my $d (@p) {
+    $min = $d if $min > $d;
+    $max = $d if $max < $d;
+  }
+  $XX{$x,$y,$z}++;
+  $XX{$x+1,$y,$z}++;
+  $YY{$x,$y,$z}++;
+  $YY{$x,$y+1,$z}++;
+  $ZZ{$x,$y,$z}++;
+  $ZZ{$x,$y,$z+1}++;
+}
+
+for my $i (values(%XX), values(%YY), values(%ZZ)) {
+  $sum++ if $i == 1;
+}
+
+out($sum);
+
+my %SEEN;
+
+my @WALLS=(\%XX,\%YY,\%ZZ);
+
+sub floodfill {
+  my $pos = shift;
+  #out($pos);
+  $SEEN{join($;, @$pos)}++;
+  for my $dim (0..2) {
+    for my $d (-1,1) {
+      my @npos = @$pos;
+      $npos[$dim] += $d;
+      next if ($npos[$dim] < $min-1 || $npos[$dim] > $max+2);
+      my $wallidx = $d > 0? join($;, @npos) : join($;, @$pos);
+      if ($WALLS[$dim]{$wallidx}) {
+        $WALLS[$dim]{$wallidx} = -1;
+        next;
+      }
+      next if ($SEEN{join($;, @npos)});
+      floodfill(\@npos);
+    }
+  }
+}
+
+floodfill([0,0,0]);
+
+$sum = 0;
+for my $i (values(%XX), values(%YY), values(%ZZ)) {
+  $sum++ if $i == -1;
+}
+
+#out (\@WALLS);
+
+out ($sum);
