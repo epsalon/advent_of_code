43761c43a60f02ee36d9f7661d683d997c309b8b
Alon Altman <github@8ln.org>
2023-12-01 21:30:06 -0800

    cleaner? code

--- 2023/02.pl
+++ 2023/02.pl
@@ -21,216 +21,24 @@ sub out {
   }
 }
 
-# Utility function to make sure 2d array has equal rows
-# input arr,fill. no output
-sub equalize {
-  my $arr = shift;
-  my $fill = shift;
-  my $rows = @$arr;
-  my $cols = max(map {$_ ? scalar(@$_): 0} @$arr);
-  print "$rows $cols\n";
-  for my $row (@$arr) {
-    $row = [] unless $row;
-    while (@$row < $cols) {
-      push @$row, $fill;
-    }
-  }
-}
+my $sumA=0;
+my $sumB=0;
 
-# Find neighbors
-# input neigh_arr, arr, row, col
-# OR: neigh_arr, arr, "row,col"
-# OR: neigh_arr, rows, cols, row, col
-# OR: neigh_arr, rows, cols, "row,col"
-# returns array of [row, col, value]
-# OR: array of ["row,col", value]
-# OR: array of "row,col"
-sub neigh {
-  my $neigh = shift;
-  my ($rows,$cols);
-  my $arr = shift;
-  if (ref $arr) {
-    $rows = @$arr;
-    $cols = @{$arr->[0]};
-  } else {
-    $rows = $arr;
-    $cols = shift;
-    undef $arr;
-  }
-  my $row = shift;
-  my $col = shift;
-  my $comma;
-  if ($row =~ /(\d+)(\D+)(\d+)/) {
-    ($row, $comma, $col) = ($1, $2, $3);
-  }
-  my @out;
-  for my $pair (@$neigh) {
-    my ($rd, $cd) = @$pair;
-    my ($nr, $nc) = ($row + $rd, $col + $cd);
-    next if $nr < 0;
-    next if $nc < 0;
-    next if $nr >= $rows;
-    next if $nc >= $cols;
-    if (defined($comma)) {
-      if ($arr) {
-        push @out, ["$nr$comma$nc", $arr->[$nr][$nc]];
-      } else {
-        push @out, "$nr$comma$nc";
-      }
-    } else {
-      push @out, [$nr, $nc, $arr ? ($arr->[$nr][$nc],) : ()];
-    }
-  }
-  return @out;
-}
+my %L = qw/red 12 green 13 blue 14/;
 
-# Orthogonal
-sub oneigh {
-  return neigh([[-1,0], [1, 0], [0, -1], [0, 1]], @_);
-}
-
-# All neighbors
-sub aneigh {
-  return neigh([
-    [-1, -1], [-1, 0], [-1, 1],
-    [ 0, -1],          [ 0, 1],
-    [ 1, -1], [ 1, 0], [ 1, 1]], @_);
-}
-
-# Numeric sort because sort defaults to lex
-# returns new array
-sub nsort {
-  my $in = \@_;
-  if (@$in == 1) {
-    $in = $in->[0];
-  }
-  return sort {$a <=> $b} @$in;
-}
-
-# Binary conversions
-sub bin2dec {
-  my $in = shift;
-  return oct("0b$in");
-}
-sub dec2bin {
-  my $in = shift;
-  return sprintf ("%b", $in);
-}
-sub hex2bin {
-  my $in = shift;
-  return join('', map {sprintf("%04b", oct("0x$_"))} split('', $in));
-}
-sub bin2hex {
-  my $in = shift;
-  my $out;
-  my @in = split('', $in);
-  die "Bad bin value $in" if (@in%4);
-  while (@in) {
-    $out .= sprintf("%X", bin2dec(join('',splice(@in, 0, 4))));
-  }
-  return $out;
-}
-
-# A* / BFS implementation
-# Args: start, end, neighbor function, heuristic function
-#  - start is either a node or a list of nodes to start at.
-#  - end is either a node or a function that takes a single node
-#    and returns true/false.
-# - neighbor function: node -> ([new_node, cost], ...)
-#   - cost assumed 1 if missing
-# - heuristic function: node -> lower bound on cost to end (optional)
-sub astar {
-  my ($start, $end, $neigh, $h) = @_;
-  # Generalize parameters
-  $start = [$start] unless (ref($start) eq 'ARRAY');
-  if (ref($end) ne 'CODE') {
-    my $end_node = $end;
-    $end = sub { return $_[0] eq $end_node; };
-  }
-  die "bad neigh func $neigh" unless (ref($neigh) eq 'CODE');
-  $h = sub {return 0;} unless $h;
-
-  # Initialize open list
-  my $OPEN = new List::PriorityQueue;
-  my %gscore;
-  my %OHASH;
-  for my $s (@$start) {
-    $gscore{$s} = 0;
-    $OHASH{$s} = 1;
-    $OPEN->insert($s, $h->($s));
-  }
-
-  my %path;
-  while (%OHASH) {
-    my $cur = $OPEN->pop();
-    delete $OHASH{$cur};
-    # Check for end
-    if ($end->($cur)) {
-      say "reached end at $cur";
-      my $score = $gscore{$cur};
-      return $score unless wantarray;
-      my @path = ($cur);
-      while ($cur = $path{$cur}) {
-        unshift(@path, $cur)
-      }
-      return ($score, @path);
-    }
-    # Expand neighbors
-    for my $n ($neigh->($cur)) {
-      my ($np,$v);
-      if (ref($n) eq 'ARRAY') {
-        ($np,$v) = @$n;
-      } else {
-        $np = $n;
-      }
-      if (!defined($v)) {
-        $v = 1;
-      }
-      my $new_g = $gscore{$cur} + $v;
-      if (!exists($gscore{$np}) || $new_g < $gscore{$np}) {
-        # Found better path to $np
-        $path{$np} = $cur if wantarray;
-        $gscore{$np} = $new_g;
-        my $fscore = $new_g + $h->($np);
-        if (!$OHASH{$np}) {
-          $OPEN->insert($np, $fscore);
-          $OHASH{$np}++;
-        } else {
-          $OPEN->update($np, $fscore);
-        }
-      }
-    }
-  }
-}
-
-sub smart_split {
-  my $str = shift || $_;
-  return ($str =~ m{[a-zA-Z]+|\d+}go);
-}
-
-sub hashify {
-  my @arr = ref $_[0] ? @{$_[0]} : @_;
-  return map {$_ => 1} @arr;
-}
-
-my @A;
-my $sum=0;
-
-LOOP: while (<>) {
+while (<>) {
   chomp;
   last unless $_;
-  m{^Game (\d+): (.+)$}go;
-  my ($g, $l) = ($1, $2);
-  my @G = split(/;/, $l);
+  m{^Game (\d+)}go;
+  my $g = $1;
   my %H;
-  for my $gg (@G) {
-    my @LL = split(/,/, $gg);
-    for my $ll (@LL) {
-      $ll =~ m{\s*(\d+)\s*([a-z]+)\s*}go;
-      $H{$2}=$1 if (!$H{$2} || $H{$2} < $1);
-    }
+  while (m{(\d+) (\w+)}go) {
+    $H{$2} = $1 if (!$H{$2} || $H{$2} < $1);
+    $g = 0 if ($L{$2} < $1);
   }
-  $sum += $H{red} * $H{blue} * $H{green};
+  $sumA += $g;
+  $sumB += product(values %H);
 }
 
-out ($sum);
+out ($sumA);
+out ($sumB);
