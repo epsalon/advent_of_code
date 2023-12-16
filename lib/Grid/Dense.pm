10bc0f4a0caf8d5d154e990bff77f754aa890e04
Alon Altman <github@8ln.org>
2023-12-16 14:06:02 -0800

    bounds function

--- lib/Grid/Dense.pm
+++ lib/Grid/Dense.pm
@@ -7,107 +7,118 @@ use Storable qw(dclone);
 use Carp;
 
 sub new {
-    my ($class, $array) = @_;
-    return bless {
-        transpose => 0,
-        flipH => 0,
-        flipV => 0,
-        data => dclone($array),
-        data_rows => scalar(@{$array}),
-        data_cols => scalar(@{$array->[0]}),
-    };
+  my ($class, $array) = @_;
+  return bless {
+      transpose => 0,
+      flipH => 0,
+      flipV => 0,
+      data => dclone($array),
+      data_rows => scalar(@{$array}),
+      data_cols => scalar(@{$array->[0]}),
+  };
 }
 
 sub read {
-    my ($class, $fh) = @_;
-    $fh = \*ARGV unless $fh;
-    my @A;
-    while (my $line = <$fh>) {
-        chomp $line;
-        last unless $line;
-        push @A, [split('', $line)];
-    }
-    return new($class, \@A);
+  my ($class, $fh) = @_;
+  $fh = \*ARGV unless $fh;
+  my @A;
+  while (my $line = <$fh>) {
+      chomp $line;
+      last unless $line;
+      push @A, [split('', $line)];
+  }
+  return new($class, \@A);
 }
 
 sub transpose {
-    my $self = shift;
-    ($self->{transpose}, $self->{flipH}, $self->{flipV}) =
-        (!$self->{transpose}, $self->{flipV}, $self->{flipH});
-    return $self;
+  my $self = shift;
+  ($self->{transpose}, $self->{flipH}, $self->{flipV}) =
+      (!$self->{transpose}, $self->{flipV}, $self->{flipH});
+  return $self;
 }
 
 sub flipH {
-    my $self = shift;
-    $self->{flipH} = !$self->{flipH};
-    return $self;
+  my $self = shift;
+  $self->{flipH} = !$self->{flipH};
+  return $self;
 }
 
 sub flipV {
-    my $self = shift;
-    $self->{flipV} = !$self->{flipV};
-    return $self;
+  my $self = shift;
+  $self->{flipV} = !$self->{flipV};
+  return $self;
 }
 
 sub rot90R {
-    my $self = shift;
-    ($self->{transpose}, $self->{flipH}, $self->{flipV}) =
-        (!$self->{transpose}, !$self->{flipV}, $self->{flipH});
-    return $self;
+  my $self = shift;
+  ($self->{transpose}, $self->{flipH}, $self->{flipV}) =
+      (!$self->{transpose}, !$self->{flipV}, $self->{flipH});
+  return $self;
 }
 
 sub rot90L {
-    my $self = shift;
-    ($self->{transpose}, $self->{flipH}, $self->{flipV}) =
-        (!$self->{transpose}, $self->{flipV}, !$self->{flipH});
-    return $self;
+  my $self = shift;
+  ($self->{transpose}, $self->{flipH}, $self->{flipV}) =
+      (!$self->{transpose}, $self->{flipV}, !$self->{flipH});
+  return $self;
 }
 
 sub rot180 {
-    my $self = shift;
-    ($self->{flipH}, $self->{flipV}) = (!$self->{flipH}, !$self->{flipV});
-    return $self;
+  my $self = shift;
+  ($self->{flipH}, $self->{flipV}) = (!$self->{flipH}, !$self->{flipV});
+  return $self;
 }
 
 sub rows {
-    my $self = shift;
-    return $self->{transpose} ? $self->{data_cols} : $self->{data_rows};
+  my $self = shift;
+  return $self->{transpose} ? $self->{data_cols} : $self->{data_rows};
 }
 
 sub cols {
-    my $self = shift;
-    return $self->{transpose} ? $self->{data_rows} : $self->{data_cols};
+  my $self = shift;
+  return $self->{transpose} ? $self->{data_rows} : $self->{data_cols};
 }
 
 sub translate_coord {
-    my $self = shift;
-    my $r = shift;
-    my $c = shift;
-    $r = $self->rows() - 1 - $r if $self->{flipV};
-    $c = $self->cols() - 1 - $c if $self->{flipH};
-    ($r,$c) = ($c,$r) if ($self->{transpose});
-    if ($r < 0 || $c < 0 || $r >= $self->{data_rows} || $c >= $self->{data_cols}) {
-      croak("[$r,$c] out of bounds");
-    }
-    return ($r,$c);
+  my $self = shift;
+  my $r = shift;
+  my $c = shift;
+  $r = $self->rows() - 1 - $r if $self->{flipV};
+  $c = $self->cols() - 1 - $c if $self->{flipH};
+  ($r,$c) = ($c,$r) if ($self->{transpose});
+  if ($r < 0 || $c < 0 || $r >= $self->{data_rows} || $c >= $self->{data_cols}) {
+    croak("[$r,$c] out of bounds");
+  }
+  return ($r,$c);
+}
+
+sub bounds {
+  my $self = shift;
+  my $r = shift;
+  my $c = shift;
+  ($r,$c) = ($c,$r) if ($self->{transpose});
+  if ($r < 0 || $c < 0 || $r >= $self->{data_rows} || $c >= $self->{data_cols}) {
+    return undef;
+  }
+  return 1;
 }
 
 sub at {
-    my $self = shift;
-    my $r = shift;
-    my $c = shift;
-    ($r,$c) = $self->translate_coord($r,$c);
-    return $self->{data}[$r][$c];
+  my $self = shift;
+  my $r = shift;
+  my $c = shift;
+  ($r,$c) = $self->translate_coord($r,$c);
+  return $self->{data}[$r][$c];
 }
 
 sub set {
-    my $self = shift;
-    my $r = shift;
-    my $c = shift;
-    my $v = shift;
-    ($r,$c) = $self->translate_coord($r,$c);
-    $self->{data}[$r][$c] = $v;
-    return $self;
+  my $self = shift;
+  my $r = shift;
+  my $c = shift;
+  my $v = shift;
+  ($r,$c) = $self->translate_coord($r,$c);
+  $self->{data}[$r][$c] = $v;
+  return $self;
 }
 
 sub to_str {
@@ -172,14 +183,14 @@ sub print {
 }
 
 sub iterate {
-    my $self = shift;
-    my $iterator = shift;
-    for my $r (0..$self->rows()-1) {
-        for my $c (0..$self->cols()-1) {
-            my $v = $self->at($r,$c);
-            $iterator->($r, $c, $v);
-        }
-    }
+  my $self = shift;
+  my $iterator = shift;
+  for my $r (0..$self->rows()-1) {
+      for my $c (0..$self->cols()-1) {
+          my $v = $self->at($r,$c);
+          $iterator->($r, $c, $v);
+      }
+  }
 }
 
 sub as_array {
@@ -205,7 +216,6 @@ sub as_array {
 sub neigh {
   my $self = shift;
   my $neigh = shift;
-  my ($rows,$cols)=($self->rows(),$self->cols);
   my $row = shift;
   my $col = shift;
   my $comma;
@@ -216,10 +226,7 @@ sub neigh {
   for my $pair (@$neigh) {
     my ($rd, $cd) = @$pair;
     my ($nr, $nc) = ($row + $rd, $col + $cd);
-    next if $nr < 0;
-    next if $nc < 0;
-    next if $nr >= $rows;
-    next if $nc >= $cols;
+    next unless $self->bounds($nr, $nc);
     if (defined($comma)) {
       push @out, ["$nr$comma$nc", $self->at($nr,$nc)];
     } else {
