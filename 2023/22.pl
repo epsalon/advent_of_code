f876145b0a2b1fdc5d3a8826e9c316743b58f831
Alon Altman <github@8ln.org>
2023-12-21 21:48:26 -0800

    racing code

--- 2023/22.pl
+++ 2023/22.pl
@@ -24,12 +24,116 @@ my @A;
 my %H;
 my $sum=0;
 
+my %G;
 #my $grid = Grid::Dense->read();
 
 while (<>) {
   chomp;
   last unless $_;
-
+  my($a,$b) = split('~');
+  my @a = split(',', $a);
+  my @b = split(',', $b);
+  for my $i ($a[0]..$b[0]) {
+    for my $j ($a[1]..$b[1]) {
+      for my $k ($a[2]..$b[2]) {
+        $G{$i,$j,$k} = @A;
+      }
+    }
+  }
+  push @A,[$a,$b];
 }
 
-out ($sum);
+sub fall() {
+  my $d=1;
+  my %fell;
+  while ($d) {
+    $d=0;
+    BLOOP: for my $bi (0..$#A) {
+      my $b = $A[$bi];
+      next unless $b;
+      my @a = split(',' ,$b->[0]);
+      my @b = split(',' ,$b->[1]);
+      for my $i ($a[0]..$b[0]) {
+        for my $j ($a[1]..$b[1]) {
+          for my $k ($a[2]..$b[2]) {
+            next BLOOP unless $k;
+            next BLOOP if defined($G{$i,$j,$k-1}) && $G{$i,$j,$k-1} != $bi;
+          }
+        }
+      }
+      $fell{$bi}++;
+      for my $i ($a[0]..$b[0]) {
+        for my $j ($a[1]..$b[1]) {
+          for my $k ($a[2]..$b[2]) {
+            delete $G{$i,$j,$k};
+          }
+        }
+      }
+      $a[2]--; $b[2]--;
+      $A[$bi] = [join(',',@a), join(',',@b)];
+      for my $i ($a[0]..$b[0]) {
+        for my $j ($a[1]..$b[1]) {
+          for my $k ($a[2]..$b[2]) {
+            $G{$i,$j,$k}=$bi;
+          }
+        }
+      }
+      $d=1;
+    }
+  }
+  return scalar(%fell);
+}
+fall();
+
+BLOOP2: for my $bi (0..$#A) {
+  my $b = $A[$bi];
+  my @a = split(',' ,$b->[0]);
+  my @b = split(',' ,$b->[1]);
+  my %sup;
+  for my $i ($a[0]..$b[0]) {
+    for my $j ($a[1]..$b[1]) {
+      for my $k ($a[2]..$b[2]) {
+        next BLOOP2 unless $k;
+        if (defined($G{$i,$j,$k-1}) && $G{$i,$j,$k-1} != $bi) {
+          $sup{$G{$i,$j,$k-1}}++;
+        }
+      }
+    }
+  }
+  die unless %sup;
+  if (%sup == 1) {
+    my ($support) = keys(%sup);
+    $H{$support}++;
+    say "brick $support cannot be disintegrated because of $bi";
+  } else {
+    say "brick $bi is supported by ", join(',', keys(%sup));
+  }
+}
+
+#out(\%G);
+
+out (@A - %H);
+
+for my $bi (0..$#A) {
+  dbg($bi);
+  my @Atmp = @A;
+  my %Gtmp = %G;
+  my $b = $A[$bi];
+  $A[$bi] = undef;
+  my @a = split(',' ,$b->[0]);
+  my @b = split(',' ,$b->[1]);
+  for my $i ($a[0]..$b[0]) {
+    for my $j ($a[1]..$b[1]) {
+      for my $k ($a[2]..$b[2]) {
+        delete ($G{$i,$j,$k});
+      }
+    }
+  }
+  my $fall=fall();
+  say "fall($bi)=$fall";
+  $sum+=$fall;
+  @A=@Atmp;
+  %G=%Gtmp;
+}
+
+out($sum);
