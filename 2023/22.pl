0dbb040f4f88caba0b97eeeed4588840b257b47c
Alon Altman <github@8ln.org>
2023-12-22 14:26:52 -0800

    much faster code

--- 2023/22.pl
+++ 2023/22.pl
@@ -8,84 +8,57 @@ use AOC ':all';
 $AOC::DEBUG_ENABLED=1;
 $|=1;
 
-my @A;
-my $sum=0;
+my @bricks;
 
-my %G;
-
-# Read input, initialize grid
+# Read input, calcualte cover
 while (<>) {
   chomp;
   last unless $_;
+  my @cover;
   my($a,$b) = split('~');
   my @a = split(',', $a);
   my @b = split(',', $b);
   for my $i ($a[0]..$b[0]) {
     for my $j ($a[1]..$b[1]) {
-      for my $k ($a[2]..$b[2]) {
-        $G{$i,$j,$k} = @A;
-      }
+      push @cover, "$i$;$j";
     }
   }
-  push @A,[$a,$b];
+  my ($min,$max) = ($a[2] < $b[2] ? ($a[2], $b[2]) : ($b[2], $a[2]));
+  push @bricks,[$min,$max,@cover];
 }
 
-# Initial fall
-my $d=1;
-while ($d) {
-  $d=0;
-  BLOOP: for my $bi (0..$#A) {
-    my $b = $A[$bi];
-    next unless $b;
-    my @a = split(',' ,$b->[0]);
-    my @b = split(',' ,$b->[1]);
-    for my $i ($a[0]..$b[0]) {
-      for my $j ($a[1]..$b[1]) {
-        for my $k ($a[2]..$b[2]) {
-          next BLOOP unless $k;
-          next BLOOP if defined($G{$i,$j,$k-1}) && $G{$i,$j,$k-1} != $bi;
-        }
-      }
-    }
-    for my $i ($a[0]..$b[0]) {
-      for my $j ($a[1]..$b[1]) {
-        for my $k ($a[2]..$b[2]) {
-          delete $G{$i,$j,$k};
-        }
-      }
-    }
-    $a[2]--; $b[2]--;
-    $A[$bi] = [join(',',@a), join(',',@b)];
-    for my $i ($a[0]..$b[0]) {
-      for my $j ($a[1]..$b[1]) {
-        for my $k ($a[2]..$b[2]) {
-          $G{$i,$j,$k}=$bi;
-        }
-      }
-    }
-    $d=1;
-  }
-}
+# Sort by bottom of brick
+@bricks = sort {$a->[0] <=> $b->[0]} @bricks;
 
-# Construct support graph
 my @support_count;
 my @supported; # supported {x} = list of bricks that x supports
-BLOOP2: for my $bi (0..$#A) {
-  my $b = $A[$bi];
-  my @a = split(',' ,$b->[0]);
-  my @b = split(',' ,$b->[1]);
-  my %sup;
-  for my $i ($a[0]..$b[0]) {
-    for my $j ($a[1]..$b[1]) {
-      for my $k ($a[2]..$b[2]) {
-        next BLOOP2 unless $k;
-        if (defined($G{$i,$j,$k-1}) && $G{$i,$j,$k-1} != $bi) {
-          $sup{$G{$i,$j,$k-1}}++;
-        }
+my %G;
+
+# Much more efficient drop, and calculate support
+for my $bi (0..$#bricks) {
+  my $b = $bricks[$bi];
+  my ($min,$max,@cover) = @$b;
+  # Drop
+  DZ: for my $dz (1..$min) {
+    my $z = $min-$dz;
+    for my $c (@cover) {
+      if (defined($G{$c,$z}) || !$z) {
+        $min-=$dz-1; $max-=$dz-1;
+        last DZ;
       }
     }
   }
-  die unless %sup;
+  $bricks[$bi] = [$min,$max,@cover];
+  # Update pit and supports
+  my %sup;
+  for my $c (@cover) {
+    if (defined(my $base = $G{$c,$min-1})) {
+      $sup{$base}++;
+    }
+    for my $z ($min..$max) {
+      $G{$c,$z}=$bi;
+    }
+  }
   $support_count[$bi] = %sup;
   for my $s (keys(%sup)) {
     push @{$supported[$s]}, $bi;
@@ -94,7 +67,8 @@ BLOOP2: for my $bi (0..$#A) {
 
 # Count drops
 my $sumA;
-for my $bi (0..$#A) {
+my $sum;
+for my $bi (0..$#bricks) {
   my $drops=0;
   my %support_loss;
   my @open = @{($supported[$bi] // [])};
