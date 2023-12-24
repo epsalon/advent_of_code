bdaea176f7065039fc9fa080b7f30c7298c99140
Alon Altman <github@8ln.org>
2023-12-24 13:23:26 -0800

    autosubmit 25b

--- 2023/submit.py
+++ 2023/submit.py
@@ -2,6 +2,9 @@
 
 import sys
 
+from aocd.get import current_day
 from aocd import submit
 
 submit(sys.argv[1])
+if current_day() == 25:
+  submit(0, part="b", day=25)
--- 2023/submit_25.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import sys
-
-from aocd import submit
-
-submit(0, part="b", year=2023, day=25)
